822e806275884072c4ea0587511592d50de521d6
Kiran <karan53wadare@gmail.com>
2021-05-31 19:18:04 +0530

    nodemon to node and instead of 4000 PORT is used

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 
 const app=express();
+const PORT = process.env.PORT || 4000;
 app.use(express.json())
 app.use(express.urlencoded())
 const mytodo = [{
@@ -23,4 +24,4 @@ app.get("/api",(req,res)=>{
     res.json(mytodo)
 })
 
-app.listen(4000,()=>console.log("server is running"))
+app.listen(PORT,()=>console.log("server is running"))
